a4539f4786d0ad4fc5f1d2f38b31aa157de84d34
smudgy-g <adamgriff86@gmail.com>
2023-11-28 15:02:29 +0100

    feat: dark/light theme button

--- app/components/global/NavBar.tsx
+++ app/components/global/NavBar.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import { Disclosure } from "@headlessui/react"
+import { Transition, Popover } from "@headlessui/react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { Fragment } from 'react'
+import ThemeButton from "./ThemeButton"
 
 export default function NavBar() {
   let pathname = usePathname() || "/"
 
   return (
-    <Disclosure as ="nav">
+    <Popover as ="nav" className="relative">
       {({open}) => (
         <>
           <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -50,11 +52,13 @@ export default function NavBar() {
                   >
                     Projects
                   </Link>
+                  <ThemeButton />
                 </div>
               </div>
 
               <div className="-mr-2 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus: ring-purple-700 dark:hover:bg-gray-800">
+                <ThemeButton classWrapper="mr-3"/>
+                <Popover.Button className="z-50 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus: ring-purple-700 dark:hover:bg-gray-800">
                   {open ? (
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -64,44 +68,54 @@ export default function NavBar() {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
                   )}
-                </Disclosure.Button>
+                </Popover.Button>
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="pt-2 pb-3 space-y-1">
-              <Link 
-                href="/" 
-                prefetch 
-                className={pathname === '/' 
-                ? 'bg-purple-50 border-purple-700 text-purple-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800' 
-                : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white'}
-              >
-                Home
-              </Link>
-              <Link 
-                href="/blog" 
-                prefetch 
-                className={pathname === '/blog' 
-                ? 'bg-purple-50 border-purple-700 text-purple-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800' 
-                : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white'}
-              >
-                Blog
-              </Link>
-              <Link 
-                href="/projects" 
-                prefetch 
-                className={pathname === '/projects' 
-                ? 'bg-purple-50 border-purple-700 text-purple-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800' 
-                : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white'}
-              >
-                Projects
-              </Link>
-            </div>
-          </Disclosure.Panel>
+          
+          <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+            <Popover.Panel className="absolute z-10 top-0 bottom-0 left-0 right-0 sm:hidden">
+              <div className="bg-white dark:bg-[#090908] pt-20 pb-3 space-y-1">
+                <Link 
+                  href="/" 
+                  prefetch 
+                  className={pathname === '/' 
+                  ? 'bg-purple-50 border-purple-700 text-purple-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800' 
+                  : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white'}
+                >
+                  Home
+                </Link>
+                <Link 
+                  href="/blog" 
+                  prefetch 
+                  className={pathname === '/blog' 
+                  ? 'bg-purple-50 border-purple-700 text-purple-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800' 
+                  : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white'}
+                >
+                  Blog
+                </Link>
+                <Link 
+                  href="/projects" 
+                  prefetch 
+                  className={pathname === '/projects' 
+                  ? 'bg-purple-50 border-purple-700 text-purple-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium dark:bg-gray-800' 
+                  : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-white block pl-3 pr-4 py-2 dark:hover:bg-gray-700 border-l-4 text-base font-medium dark:text-white'}
+                >
+                  Projects
+                </Link>
+              </div>
+            </Popover.Panel>
+          </Transition>
         </>
       )}
-    </Disclosure>
+    </Popover>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './components/global/NavBar'
+import { Provider } from './components/global/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-white text-gray-900 dark:bg-[#090908] dark:text-white h-full selection:bg-gray-50 dark:selection:bg-gray-800`}>
-        <NavBar />
-        {children}
+        <Provider>
+          <NavBar />
+          <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
