7c92c8bc450586431ba3dd5af9d734a1c93afc50
Adam James Griffiths <adamgriff86@gmail.com>
2023-12-15 08:09:21 +0100

    Update projects.ts

--- db/projects.ts
+++ db/projects.ts
@@ -1,6 +1,6 @@
 const projects = [
   {
-    "description": "An AI-powered personal journal that itegrates Ai and Large Language Models to track the users mood through analysis of each journal entry with mapping to a chart.  Users also have the posibility to query for insights into their ournal history.",
+    "description": "An AI-powered personal journal that itegrates Ai and Large Language Models to track the users mood through analysis of each journal entry with mapping to a chart. Users can query for insights into their journal history.",
     "name": "journai",
     "projectUrl": "https://github.com/smudgy-g/journai",
     "tagline": "AI-powered journal tracking mood & insight query"
