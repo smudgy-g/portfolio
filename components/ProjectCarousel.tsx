0f0056407a016cf59727d8418a2115ecbbbea8c7
Adam James Griffiths <adamgriff86@gmail.com>
2024-01-08 15:19:17 +0100

    Update ProjectCarousel.tsx

--- components/ProjectCarousel.tsx
+++ components/ProjectCarousel.tsx
@@ -17,14 +17,13 @@ export default function ProjectCarousel () {
   const [currentProject, setCurrentProject] = useState<Project>(projects[0])
 
   return (
-    <div className="mx-2 mt-8 md:mx-auto md:my-auto flex flex-col gap gap-2 lg:flex-row md:gap-20 items-center ">
-      
+    <div className="mx-2 mt-8 md:mx-auto md:my-auto flex flex-col gap gap-2 lg:flex-row md:gap-20 items-center">
       <div className="carousel carousel-center max-w-full md:max-w-lg p-4 space-x-4 rounded-box md:text-left">
         {projects && projects.map((project, i) => (
         <div className="carousel-item rounded-box bg-neutral" key={`project${i}`}>
           <div className="card card-compact w-56 md:w-96 bg-base-100 border shadow-xl">
             <figure>
-              <Image src={`/assets/${project.name}.png`} height={250} width={250} alt=`image for ${project.name}` className="object-none h-20 w-full md:h-40 lg:h-56"/>
+              <Image src={`/assets/${project.name}.png`} height={250} width={250} alt={`image for ${project.name}`} className="object-none h-20 w-full md:h-40 lg:h-56"/>
             </figure>
             <div className="card-body">
               <h2 className="card-title justify-center capitalize">{project.name}</h2>
@@ -44,17 +43,16 @@ export default function ProjectCarousel () {
       </div>
 
       <div className="prose px-3 md:max-w-md lg:max-w-xs text-justify lg:text-left ">
-          <h2 className="text-3xl md:text-5xl mb-3 capitalize">
-            {currentProject.name}
-            <span>
-              <a href={currentProject.projectUrl} target="_blank" className="ml-7 badge badge-secondary">
-                <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-              </a>
-            </span>
-          </h2>
+        <h2 className="text-3xl md:text-5xl mb-3 capitalize">
+          {currentProject.name}
+          <span>
+            <a href={currentProject.projectUrl} target="_blank" className="ml-7 badge badge-secondary">
+              <ArrowTopRightOnSquareIcon className="w-4 h-4" />
+            </a>
+          </span>
+        </h2>
         <p className="leading-5 md:leading-relaxed tracking-wide text-sm">{currentProject.description}</p>
-        <div className="flex justify-end md:block">
-        </div>
+        <div className="flex justify-end md:block"></div>
       </div>
     </div>
   )
