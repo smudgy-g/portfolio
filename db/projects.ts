56df60f16071015addf707cb7a2d493a64dde931
Adam James Griffiths <adamgriff86@gmail.com>
2024-01-16 09:46:02 +0100

    Update projects.ts

--- db/projects.ts
+++ db/projects.ts
@@ -1,15 +1,9 @@
 const projects = [
   {
-    "description": "An AI-powered personal journal that itegrates Ai and Large Language Models to track the users mood through analysis of each journal entry with mapping to a chart. Users can query for insights into their journal history.",
-    "name": "journai",
-    "projectUrl": "https://github.com/smudgy-g/journai",
-    "tagline": "AI-powered journal tracking mood & insight query"
-  },
-  {
-    "description": "Integrated OpenAI API and Langchain for registration form  & event planning assistance. Using LLM integration, users can gain quick analysis and insights into guests' needs and profiles.",
-    "name": "papaya",
-    "projectUrl": "https://github.com/smudgy-g/papaya",
-    "tagline": "Dynamic PageForm builder with integrated AI"
+    "description": "xhibit is a social media platform inspired by Instagram. Users can sign up, sign in, create, like, edit, save, and delete posts, as well as maintain their profiles. The app features a feed, and users can search for posts.",
+    "name": "exhibit",
+    "projectUrl": "https://github.com/smudgy-g/exhibit",
+    "tagline": "Exhibit is a social media platform inspired by Instagram."
   },
 {
     "description": "Kiosk consolidates all your ordering needs into one convenient platform, saving you time and effort. Whether you need to restock ingredients, beverages, or other supplies, kiosk has got you covered.",
@@ -24,6 +18,18 @@ const projects = [
     "tagline": "Share More, Own Less - Welcome to the Future of Lending!"
   },
   
+  {
+    "description": "An AI-powered personal journal that itegrates Ai and Large Language Models to track the users mood through analysis of each journal entry with mapping to a chart. Users can query for insights into their journal history.",
+    "name": "journai",
+    "projectUrl": "https://github.com/smudgy-g/journai",
+    "tagline": "AI-powered journal tracking mood & insight query"
+  },
+  {
+    "description": "Integrated OpenAI API and Langchain for registration form  & event planning assistance. Using LLM integration, users can gain quick analysis and insights into guests' needs and profiles.",
+    "name": "papaya",
+    "projectUrl": "https://github.com/smudgy-g/papaya",
+    "tagline": "Dynamic PageForm builder with integrated AI"
+  },
 ]
 
 export default projects
