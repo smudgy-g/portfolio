e09b84bc7a8dd004e552813116faa9260bdd5ada
Adam James Griffiths <adamgriff86@gmail.com>
2024-02-15 09:19:58 +0100

    Update projects.ts

--- db/projects.ts
+++ db/projects.ts
@@ -1,5 +1,11 @@
 const projects = [
   {
+    "description": "Kiosk consolidates all your ordering needs into one convenient platform, saving you time and effort. Whether you need to restock ingredients, beverages, or other supplies, kiosk has got you covered.",
+    "name": "kiosk",
+    "projectUrl": "https://kyosk.vercel.app/",
+    "tagline": "The Mobile-Friendly Inventory Management App for Restaurants and Bars"
+  },
+  {
     "description": "Exhibit is a social media platform inspired by Instagram. Users can sign up, sign in, create, like, edit, save, and delete posts, as well as maintain their profiles. The app features a feed, and users can search for posts.",
     "name": "exhibit",
     "projectUrl": "https://exhibit-one.vercel.app/",
@@ -11,19 +17,12 @@ const projects = [
     "projectUrl": "https://rendez.vercel.app/",
     "description": "The service allows users to browse, create, and promote local events. Users are able to log in and purchase tickets to events securely and access related events, past orders and create and edit local events."
   },
-{
-    "description": "Kiosk consolidates all your ordering needs into one convenient platform, saving you time and effort. Whether you need to restock ingredients, beverages, or other supplies, kiosk has got you covered.",
-    "name": "kiosk",
-    "projectUrl": "https://kyosk.vercel.app/",
-    "tagline": "The Mobile-Friendly Inventory Management App for Restaurants and Bars"
-  },
-{
+  {
     "description": "Lendify is a community marketplace platform with a focus on lending where users can leverage what they have and access what they need while earning credits and saving money. We strive to revolutionize the way people use and share everyday items, making ownership an option, not a necessity.",
     "name": "lendify",
     "projectUrl": "https://github.com/kevin-kowalski/lendify",
     "tagline": "Share More, Own Less - Welcome to the Future of Lending!"
   },
-  
   {
     "description": "An AI-powered personal journal that itegrates Ai and Large Language Models to track the users mood through analysis of each journal entry with mapping to a chart. Users can query for insights into their journal history.",
     "name": "journai",
